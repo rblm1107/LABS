8228157ab572614691bcfb4feb66f71d4d34d88b
Rahma Belmustapha <rbelmustapha@gmail.com>
2019-04-15 11:56:32 +0200

    le fichier footer pareil que le header

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- Footer section end -->
+<footer class="footer-section">
+    <h2>2017 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
+</footer>
+
+
+<!--====== Javascripts & Jquery ======-->
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/magnific-popup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/circle-progress.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -422,21 +422,4 @@
 
 
 <!-- Footer section -->
-<footer class="footer-section">
-    <h2>2017 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
-</footer>
-<!-- Footer section end -->
-
-
-
-
-<!--====== Javascripts & Jquery ======-->
-<script src="js/jquery-2.1.4.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/magnific-popup.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/circle-progress.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-
-</html>
+<?php get_footer();?>
