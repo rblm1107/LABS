85469f233c8443dc11ab59fcd388875400c37476
Rahma Belmustapha <rbelmustapha@gmail.com>
2019-04-15 11:49:20 +0200

    on séparer le header dans un fichier seul pour que toutes les pages puisse l'utiliser et qu'il ne faille modifier le header qu'à un seul endroit.

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Labs - Design Studio</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Labs - Design Studio">
+    <meta name="keywords" content="lab, onepage, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="shortcut icon" />
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,700" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/flaticon.css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <link rel="stylesheet" href="css/owl.carousel.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
+
+    <!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader">
+            <img src="img/logo.png" alt="">
+            <h2>Loading.....</h2>
+        </div>
+    </div>
+
+
+    <!-- Header section -->
+    <header class="header-section">
+        <div class="logo">
+            <img src="img/logo.png" alt=""><!-- Logo -->
+        </div>
+        <!-- Navigation -->
+        <div class="responsive"><i class="fa fa-bars"></i></div>
+        <nav>
+            <ul class="menu-list">
+                <li class="active"><a href="home.html">Home</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="elements.html">Elements</a></li>
+            </ul>
+        </nav>
