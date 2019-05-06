be2d681ae7bb2d4858e51ea91dce550ec39604b0
Rahma Belmustapha <rbelmustapha@gmail.com>
2019-05-06 14:26:46 +0200

    ajouter de dynamique posts dans la section about

--- index.php
+++ index.php
@@ -35,35 +35,26 @@ $second_image = get_theme_mod('second-image');
         <div class="container">
             <div class="row">
                 <!-- single card -->
-                <div class="col-md-4 col-sm-6">
-                    <div class="lab-card">
-                        <div class="icon">
-                            <i class="flaticon-023-flask"></i>
+                <?php
+                $args = [
+                    'post_type' => 'services',
+                    'posts_per_page' => 3,
+                    // 'paged' => $paged,
+                    // 'orderby'        => 'rand',
+                ];
+                $query = new WP_Query($args);
+                while ($query->have_posts()) : $query->the_post(); ?>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="lab-card">
+                            <div class="icon">
+                                <i class="<?php echo get_post_meta(get_the_ID(), 'icon', true); ?>"></i>
+                            </div>
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_content(); ?></p>
                         </div>
-                        <h2>Get in the lab</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
                     </div>
-                </div>
-                <!-- single card -->
-                <div class="col-md-4 col-sm-6">
-                    <div class="lab-card">
-                        <div class="icon">
-                            <i class="flaticon-011-compass"></i>
-                        </div>
-                        <h2>Projects online</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
-                    </div>
-                </div>
-                <!-- single card -->
-                <div class="col-md-4 col-sm-12">
-                    <div class="lab-card">
-                        <div class="icon">
-                            <i class="flaticon-037-idea"></i>
-                        </div>
-                        <h2>SMART MARKETING</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
-                    </div>
-                </div>
+                <?php endwhile; ?>
+
             </div>
         </div>
     </div>
